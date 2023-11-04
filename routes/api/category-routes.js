2d2311cb34adb0e11833b4afd002e98c5e55d717
Jason Savoie <flimits@gmail.com>
2023-11-03 19:29:31 -0700

    fixed up category and all is working with it

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,47 +3,102 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  // => from model
-  // somehow send it back the the user
-  // 
+router.get('/', async (req, res) => {
   try {
+    // Use the findAll method on the Category model to fetch all categories
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }], // Include associated Products
+    });
 
+    // Send the category data back to the user
+    res.status(200).json(categoryData);
   } catch (err) {
-    
+    console.error(err);
+    res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  // get a category by its ID
-  // some req.params.nonsense
-  // send that back to user
+// Find one category by its `id` value
+// be sure to include its associated Products
+// get a category by its ID
+// some req.params.nonsense
+// send that back to user
+router.get('/:id', async (req, res) => {
+  try {
+const categoryData = await Category.findByPk(req.params.id, {
+  include: [{ model: Product }],
+});
+res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-  // get the body and get the contents and insert into sequelize
-  // category(DOT)create 
-  // whatever we created we can return it back off the res.json
+// create a new category
+// get the body and get the contents and insert into sequelize
+// category(DOT)create 
+// whatever we created we can return it back off the res.json
+router.post('/', async (req, res) => {
+  try {
+const categoryData = await Category.create({
+  category_name: req.body.category_name,
+});
+res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-  // taking in an id as a parameter and also receiving a req.body
-  // sequelize update
+// update a category by its `id` value
+// taking in an id as a parameter and also receiving a req.body
+// sequelize update
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (categoryData[0] === 0) {
+      // Handle the case where no rows were updated
+      res.status(404).json({ message: 'Category not found' });
+    } else {
+      res.status(200).json({ message: 'Category updated successfully' });
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-  // DESTROY
-  // destroying based off the req.params.id
-  // res.json to let the server know it's gone
-  // you have to send something back in order for it to close
+
+// delete a category by its `id` value
+// DESTROY
+// destroying based off the req.params.id
+// res.json to let the server know it's gone
+// you have to send something back in order for it to close
+router.delete('/:id', async (req, res) => {
+try {
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!categoryData){
+    res.status(404).json({ message: 'No Category by that ID found'});
+    return;
+  }
+
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
